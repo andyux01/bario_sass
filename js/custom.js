c3fa45348903b3be761d9ae1847b08f927dab416
Andy Pembelajar <andybelajas@gmail.com>
2024-05-20 14:49:33 +0700

    belajar theming bario

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bario_sass_coba = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
